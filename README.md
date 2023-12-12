c83cf4e39529cca2034f732cf1e900c6a03ff825
ESPhoenix <e.s.phoenix@hotmail.co.uk>
2023-12-12 14:34:20 +0000

    no lig functionality

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # drMD
 Automated workflow for running molecular dynamics simulations with Amber and Openmm
+
+# Installation
+
+##Create and activate conda environment
+```bash
+conda create -n drMD python=3.10
+conda activate drMD
+```
+##Install AmberTools (needs to be before OpenMM)
+```bash
+conda install -c conda-forge ambertools=23
+``` 
+##Install OpenMM
+```bash
+conda install -c omnia openmm
+``` 
+##Install other python libraries
+```bash
+pip install argpass
+pip install pyyaml
+pip install pandas
+```
--- prep_drMD.py
+++ prep_drMD.py
@@ -3,8 +3,11 @@ import os
 from os import path as p
 import pandas as pd
 from subprocess import run
+import string
+from shutil import copy
 ## drMD UTILS
 from pdbUtils import *
+
 #####################################################################################
 def split_input_pdb(inputPdb,config,outDir):
     # read whole pdb into a df
@@ -25,11 +28,12 @@ def split_input_pdb(inputPdb,config,outDir):
 #####################################################################################
 def prepare_ligand_parameters(config, outDir):
     ligandsDict = config["ligandInfo"]["ligands"]
+    inputDir = config["pathInfo"]["inputDir"]
     ligandPdbs = []
     ligandFileDict = {}
     # for each ligand in config
     for ligand in ligandsDict:
-        fileDict = {}
+        ligFileDict = {}
         # find files and directories
         ligandName = ligand["ligandName"]
         ligPrepDir = p.join(outDir,ligandName)
@@ -41,27 +45,54 @@ def prepare_ligand_parameters(config, outDir):
             ligPdb_h = p.join(ligPrepDir,f"{ligandName}_h.pdb")
             reduceCommand = f"reduce {ligPdb} > {ligPdb_h}"
             run(reduceCommand,shell=True)
+            # rename all new hydrogens H1, H2, H3 ... (fixes 4-character names)
+            ligPdb_newH = p.join(ligPrepDir,f"{ligandName}_newH.pdb")
+            rename_hydrogens(ligPdb_h,ligPdb_newH)
             # run pdb4amber to get compatable types and fix atom numbering
             ligPdb_amber = p.join(ligPrepDir,f"{ligandName}_amber.pdb")
-            pdb4amberCommand = f"pdb4amber -i {ligPdb_h} -o {ligPdb_amber}"
+            pdb4amberCommand = f"pdb4amber -i {ligPdb_newH} -o {ligPdb_amber}"
             run(pdb4amberCommand,shell=True)
             ligPdb = ligPdb_amber
         ligandPdbs.append(ligPdb)
-        # convert to mol2 with antechamber
-        charge = ligand["charge"]
-        ligMol2 = f"{ligandName}.mol2"
-        antechamberCommand = f"antechamber -i {ligPdb} -fi pdb -o {ligMol2} -fo mol2 -c bcc -s 2 -nc {charge}"
-        run(antechamberCommand, shell=True)
+
+        if ligand["mol2"]:
+            ligMol2 = p.join(inputDir,f"{ligandName}.mol2")
+        else:
+            # convert to mol2 with antechamber
+            charge = ligand["charge"]
+            ligMol2 = p.join(ligPrepDir,f"{ligandName}.mol2")
+            antechamberCommand = f"antechamber -i {ligPdb} -fi pdb -o {ligMol2} -fo mol2 -c bcc -s 2 -nc {charge}"
+            run(antechamberCommand, shell=True)
         # add mol2  path to config 
-        fileDict.update({"mol2":ligMol2})
-        # use mol2 to generate amber parameters with parmchk2
-        ligFrcmod = f"{ligandName}.frcmod"
-        parmchk2Command = f"parmchk2 -i {ligMol2} -f mol2 -o {ligFrcmod}"
-        run(parmchk2Command,shell=True)
-        # add frcmod path to config
-        fileDict.update({"frcmod":ligFrcmod})
-        ligandFileDict.update({ligandName:fileDict})
+        ligFileDict.update({"mol2":ligMol2})
+
+        # if frcmod file is provided, don't run param generation (expensive!)
+        if ligand["toppar"]:
+            ligFrcmod = p.join(inputDir,f"{ligandName}.frcmod")
+        else :
+            # use mol2 to generate amber parameters with parmchk2
+            ligFrcmod = p.join(ligPrepDir,f"{ligandName}.frcmod")
+            parmchk2Command = f"parmchk2 -i {ligMol2} -f mol2 -o {ligFrcmod}"
+            run(parmchk2Command,shell=True)
+        # add frcmod path to ligFileDict
+        ligFileDict.update({"frcmod":ligFrcmod})
+        ligandFileDict.update({ligandName:ligFileDict})
     return ligandPdbs, ligandFileDict
+#####################################################################################
+def rename_hydrogens(pdbFile,outFile):
+    pdbDf = pdb2df(pdbFile)
+    hDf = pdbDf[pdbDf["ELEMENT"]=="H"]
+    letters = list(string.ascii_uppercase)
+    numbers = [str(i) for i in range(1,10)]
+    newNameHs = []
+    for letter in letters:
+        for number in numbers:
+            newNameHs.append("H"+letter+number)
+    count = 0
+    for index, row in hDf.iterrows():
+        pdbDf.loc[index,"ATOM_NAME"] = newNameHs[count]
+        count += 1
+    df2Pdb(pdbDf,outFile)
 #####################################################################################
 def prepare_protein_structure(config, outDir):
     proteinDict = config["proteinInfo"]["proteins"]
@@ -69,24 +100,30 @@ def prepare_protein_structure(config, outDir):
     # for each protein in config
     for protein in proteinDict:
         # find files and directories
-        protName = protein["proteinName"]
         protPrepDir = p.join(outDir,"PROT")
+        os.makedirs(protPrepDir,exist_ok=True)
         os.chdir(protPrepDir)
         protPdb = p.join(protPrepDir,"PROT.pdb")
+        # check for PROT.pdb in protPrepDir (won't be there if noLigand)
+        if not p.isfile(protPdb):
+            copy(config["pathInfo"]["inputPdb"],protPdb)
+
         if not protein["protons"]:
             # add protons with reduce
             protPdb_h = p.join(protPrepDir,"PROT_h.pdb")
             reduceCommand = f"reduce {protPdb} > {protPdb_h}"
             run(reduceCommand, shell=True)
-            # run pdb4amber to get compatable types and fix atom numbering
+            #run pdb4amber to get compatable types and fix atom numbering
             protPdb_amber = p.join(protPrepDir,"PROT_amber.pdb")
             pdb4amberCommand = f"pdb4amber -i {protPdb_h} -o {protPdb_amber}"
             run(pdb4amberCommand, shell = True)
-            protPdb = protPdb_amber
+            protPdb = protPdb
             proteinPdbs.append(protPdb)
     return proteinPdbs
+
+
 #####################################################################################
-def make_amber_params(outDir,ligandFileDict, pdbFile, outName):
+def make_amber_params(outDir, pdbFile, outName,ligandFileDict=False):
     os.chdir(outDir)
     # write tleap input file
     tleapInput = p.join(outDir, "TLEAP.in")
@@ -95,16 +132,17 @@ def make_amber_params(outDir,ligandFileDict, pdbFile, outName):
         f.write("source oldff/leaprc.ff14SB\n")
         f.write("source leaprc.gaff2\n")
         f.write("source leaprc.water.tip3p\n\n")
-        ## ligand mol2 and ff
-        for ligandName in ligandFileDict:
-            ligMol2 = ligandFileDict[ligandName]["mol2"]
-            ligFrcmod = ligandFileDict[ligandName]["frcmod"]
-            f.write(f"{ligandName} = loadmol2 {ligMol2}\n")
-            f.write(f"loadamberparams {ligFrcmod}\n\n")
+        if ligandFileDict:
+            ## ligand mol2 and ff
+            for ligandName in ligandFileDict:
+                ligMol2 = ligandFileDict[ligandName]["mol2"]
+                ligFrcmod = ligandFileDict[ligandName]["frcmod"]
+                f.write(f"{ligandName} = loadmol2 {ligMol2}\n")
+                f.write(f"loadamberparams {ligFrcmod}\n\n")
         ## ions ff
         f.write("loadamberparams frcmod.ions1lm_126_tip3p\n")
         ## whole protein structure
-        f.write(f"loadpdb {pdbFile}\n")
+        f.write(f"mol = loadpdb {pdbFile}\n")
         # solvation and ions
         f.write("solvatebox mol TIP3PBOX 10.0\n")
         f.write("addions mol Na+ 0\n")
@@ -115,5 +153,9 @@ def make_amber_params(outDir,ligandFileDict, pdbFile, outName):
         f.write(f"saveamberparm mol {outName}.prmtop {outName}.inpcrd\n")
         f.write("quit\n")
     tleapOutput = p.join(outDir,"TLEAP.out")
-    tleapCommand = f"tleap -s {tleapInput} > {tleapOutput}"
+    tleapCommand = f"tleap -f {tleapInput} > {tleapOutput}"
     run(tleapCommand,shell=True)
+    inputCoords = p.join(outDir, f"{outName}.inpcrd")
+    amberParams = p.join(outDir, f"{outName}.prmtop")
+
+    return inputCoords, amberParams
