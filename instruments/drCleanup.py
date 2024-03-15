d22c4b9e7a1a8a80680e51c5e2c58b3b464c0798
ESPhoenix <e.s.phoenix@hotmail.co.uk>
2024-03-15 16:38:21 +0000

    cleanup now working

--- instruments/drCleanup.py
+++ instruments/drCleanup.py
@@ -2,39 +2,56 @@ import os
 from os import path as p 
 from shutil import copy, rmtree
 from instruments.pdbUtils import pdb2df, df2pdb
-
 ######################################################################################################
-def get_endpoint_pdbs(simulationInfo, outDir, cleanUpInfo):
-    if "keepFileNames" in cleanUpInfo:
-        keepFileNames = cleanUpInfo["keepFileNames"]   
-    else:
-        keepFileNames = False
+def clean_up_handler(batchConfig):
+    # READ FROM batchConfig
+    simulationInfo = batchConfig["simulationInfo"]
+    pathInfo = batchConfig["pathInfo"]
+    # RUN THROUGH OPTIONS IN cleanUpInfo
+    if  not "cleanUpInfo" in batchConfig:
+        return 
+    cleanUpInfo = batchConfig["cleanUpInfo"]
+    if "getEndpointPdbs" in cleanUpInfo:
+        if cleanUpInfo["getEndpointPdbs"]:
+            get_endpoint_pdbs(simulationInfo, pathInfo)
+            if any(key in cleanUpInfo for key in ["removeWaters","removeIons"]):
+                remove_atoms_from_pdb(simulationInfo, cleanUpInfo, pathInfo)
+######################################################################################################
+def get_endpoint_pdbs(simulationInfo, pathInfo):
+    inputDir = pathInfo["inputDir"]
+    outDir = pathInfo["outputDir"]
+
+    inputNames = [p.splitext(pdbFile)[0] for pdbFile in os.listdir(inputDir) if p.splitext(pdbFile)[1] == ".pdb"]
+
+    # make a new dir to collate pdbs to 
+    collatedPdbDir = p.join(outDir, "collatedPdbs")
+    os.makedirs(collatedPdbDir,exist_ok=True)
     excudeDirNames = ["00_configs","01_ligand_parameters"]
     for sim in simulationInfo:
         stepName = sim["stepName"]
         tag = sim["type"]
-        collateDir = p.join(outDir,stepName)
-        os.makedirs(collateDir,exist_ok=True)
+        collateSubDir = p.join(collatedPdbDir,stepName)
+        os.makedirs(collateSubDir,exist_ok=True)
         excudeDirNames.append(stepName)
-        for protName in os.listdir(outDir):
-            if protName in excudeDirNames:
+        for inputName in inputNames:
+            if inputName in excudeDirNames:
                 continue
-            stepDir = p.join(outDir,protName,stepName)
+            stepDir = p.join(outDir,inputName,stepName)
             for file in os.listdir(stepDir):
                 if p.splitext(file)[1] == ".pdb":
-                    if keepFileNames: 
-                        copy(p.join(stepDir,file),p.join(collateDir,f"{protName}.pdb"))
-                    else:
-                        copy(p.join(stepDir,file),p.join(collateDir,f"{protName}_{tag}.pdb"))
+                    copy(p.join(stepDir,file),p.join(collateSubDir,f"{inputName}_{tag}.pdb"))
 ######################################################################################################
-def remove_atoms_from_pdb(simulationInfo, cleanUpInfo, outDir):
+def remove_atoms_from_pdb(simulationInfo, cleanUpInfo, pathInfo):
+    outDir = pathInfo["outputDir"]
+    collatedPdbDir = p.join(outDir, "collatedPdbs")
+
     for sim in simulationInfo:
         stepName = sim["stepName"]
-        collateDir = p.join(outDir,stepName)
-        for file in os.listdir(collateDir):
+        collateSubDir = p.join(collatedPdbDir,stepName)
+        for file in os.listdir(collateSubDir):
             if not p.splitext(file)[1] == ".pdb":
                 continue
-            pdbFile = p.join(collateDir,file)
+            pdbFile = p.join(collateSubDir,file)
             pdbDf = pdb2df(pdbFile)
             if "removeWaters" in cleanUpInfo:
                 if cleanUpInfo["removeWaters"]:
