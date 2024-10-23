d71f4268dcbfd9fc8b87a2335c72715b2f38afc4
ESPhoenix <e.s.phoenix@hotmail.co.uk>
2024-10-23 14:03:57 +0100

    GPUtil import

--- src/UtilitiesCloset/drMethodsWriter.py
+++ src/UtilitiesCloset/drMethodsWriter.py
@@ -6,7 +6,7 @@ import yaml
 from glob import glob
 import numpy as np
 import inflect
-import gputil
+import GPUtil
 
 ## PDB // DATAFRAME UTILS
 from pdbUtils import pdbUtils
@@ -71,7 +71,7 @@ def methods_writer_protocol(batchConfig: Dict, configDir: DirectoryPath, outDir:
 
 ##########################################################################################
 
-# def write_gpu_methods(methodsFile: FilePath):
+# def write_hardware_methods(methodsFile: FilePath, config: Dict) -> None:
 #     """
 #     Writes methods section talking about GPU usage, generic to all simulations run by drMD
     
@@ -81,17 +81,23 @@ def methods_writer_protocol(batchConfig: Dict, configDir: DirectoryPath, outDir:
 #     Returns:
 #         None    
 #     """
-#     gpuData = gputil.getGPUs()
+#     gpuData = GPUtil.getGPUs()
 
-
+#     hardwareInfo: dict = config["hardwareInfo"]
+#     nCpus = hardwareInfo["parallelCpus"] * hardwareInfo["subprocessCpus"]
 
 
 #     with open(methodsFile, "a") as methods:
 #         if not gpuData:
-#             methods.write("Simulations were")
+#             methods.write(f"Simulations were performed without GPU acceleration using {nCpus} CPUs.\n\n")
 
-#         methods.write("## GPU Usage\n\n")
-#         methods.write("All simulations were run on a Tesla T4 GPU. \n\n")
+#         elif len(gpuData) == 1:
+#             methods.write(f"Simulations were performed on a {gpuData[0].name} using {nCpus} CPUs.\n\n")
+#         elif len(gpuData) > 1:
+#             gpuNameList = format_list([gpu.name for gpu in gpuData])
+
+#             methods.write(f"Simulations were performed on the following GPUs: {gpuNameList} using {nCpus}\n\n\")
+
 
 def get_config_dicts(configDir: DirectoryPath) -> List[Dict]:
     """
